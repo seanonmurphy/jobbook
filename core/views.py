db1d33204c5be4abacbcc91ccaa9511bea9126d0
Sean Murphy <seanonmurphy@gmail.com>
2016-02-28 14:01:01 +0100

    Added lists of information to client and job details pages.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 # from django.contrib.auth.models import User
-from .models import Client, Job
+from .models import Client, Job, Task
 
 
 def index(request):
@@ -16,11 +16,13 @@ def clients(request):
 
     all_clients = []
     for client in clients:
+        related_jobs = Job.objects.filter(client=client)
         cli = {
             'id': client.id,
             'first_name': client.first_name,
             'last_name': client.last_name,
-            'company': client.company
+            'company': client.company,
+            'jobs_count': len(related_jobs),
         }
         all_clients.append(cli)
 
@@ -31,7 +33,13 @@ def clients(request):
 def client_detail(request, client_id):
     client = get_object_or_404(Client, pk=client_id)
 
-    context = {'client': client}
+    related_jobs = Job.objects.filter(client=client)
+    # TODO: update to show correct total time (sum time from all tasks of each job)
+
+    # TODO: get all task from all realted jobs
+    related_tasks = [Task.objects.filter(job=job) for job in related_jobs]
+
+    context = {'client': client, 'jobs': related_jobs, 'tasks': related_tasks}
     return render(request, 'core/single-client.html', context)
 
 
@@ -62,8 +70,9 @@ def jobs(request):
 
 def job_detail(request, job_id):
     job = get_object_or_404(Job, pk=job_id)
+    related_tasks = Task.objects.filter(job=job)
 
-    context = {'job': job}
+    context = {'job': job, 'tasks': related_tasks}
     return render(request, 'core/single-job.html', context)
 
 
